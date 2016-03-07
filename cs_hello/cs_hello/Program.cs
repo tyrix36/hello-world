beb4ef95427fb7499ec5e2e6ab142feff0cde572
tyrix36 <tyrix36+github@gmail.com>
2016-03-07 17:08:09 +0100

    base01
    base01

---
+++ cs_hello/cs_hello/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace cs_hello
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("debut");
+
+
+
+            Console.WriteLine("fin");
+            Console.ReadKey();
+
+
+        }
+    }
+}
